f579eee2a0e194674ced630ba7fb597255c37a4a
dennisdlr23 <106792124+dennisdlr23@users.noreply.github.com>
2023-04-15 20:10:17 -0600

    Create cron.sh

---
+++ sh/cron.sh
@@ -0,0 +1,6 @@
+30 8 * * * /home/dennisdlr/proyectoFinalIA/control.php 'gpio17=1'
+0 17 * * * /home/dennisdlr/proyectoFinalIA/control.php 'gpio17=0'
+30 9 * * * /home/dennisdlr/proyectoFinalIA/control.php 'gpio21=1'
+0 16 * * * /home/dennisdlr/proyectoFinalIA/control.php 'gpio21=0'
+30 10 * * * /home/dennisdlr/proyectoFinalIA/control.php 'gpio27=1'
+0 15 * * * /home/dennisdlr/proyectoFinalIA/control.php 'gpio27=0'
